d68ec050cfacb527b11087837f098014b69edfe8
rash0x6964 <rachidbnou572@gmail.com>
2023-07-13 12:52:30 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - is the entry point
+ * Return: 0 if succeeded
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
