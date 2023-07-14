7be3377f24a15db299f5cce9b829a121a91dcdf9
rash0x6964 <rachidbnou572@gmail.com>
2023-07-13 23:26:55 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 < 9; n1++)
+	{
+		for (n2 = n1 + 1; n2 < 10; n2++)
+		{
+			putchar((n1 % 10) + '0');
+			putchar((n2 % 10) + '0');
+			if (n1 == 8 && n2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
