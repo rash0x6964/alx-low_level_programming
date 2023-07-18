425af8cd50fc76b400d0741fb6f55beedc19d17a
rash0x6964 <rachidbnou572@gmail.com>
2023-07-18 03:53:09 -0700

    ...

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,7 +26,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(product % 10 + '0');
+					_putchar(res % 10 + '0');
 				}
 				else if (res >= 10 && res < 100)
 				{
