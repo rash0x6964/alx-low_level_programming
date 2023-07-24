efbdff06663ea4a41500e850ecbe9275952738be
rash0x6964 <rachidbnou572@gmail.com>
2023-07-24 00:18:21 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * print_rev - Prints a string in reverse
+  * @s: string param
+  * Return: void
+  */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (i -= 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
