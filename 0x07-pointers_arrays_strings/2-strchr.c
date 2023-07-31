75f0d44b01ec797274d4a0a60d0d1dc6438dcadb
rash0x6964 <rachidbnou572@gmail.com>
2023-07-31 05:06:09 -0700

    fix...

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,11 +5,12 @@
  * @c: char that looking for
  * Return: char
  */
+
 char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0 ; s[i] > '\0'; i++)
+	for (i = 0 ; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 			return (&s[i]);
