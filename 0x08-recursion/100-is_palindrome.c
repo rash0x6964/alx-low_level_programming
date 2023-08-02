5ed59cc3bd6d3fcdad77f24e91bd84214c7248f6
rash0x6964 <rachidbnou572@gmail.com>
2023-08-02 02:24:18 -0700

    task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -49,7 +49,7 @@ int check_palindrome(char *s, int len, int index)
 int is_palindrome(char *s)
 {
 	int index = 0;
-	int len = find_strlen(s);
+	int len = _strlen_recursion(s);
 
 	if (!(*s))
 		return (1);
