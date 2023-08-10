12303f98a1eab802ddb185c0e7f3a664b0827ba6
rash0x6964 <rachidbnou572@gmail.com>
2023-08-10 07:06:41 -0700

    task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * string_nconcat - Concatenates two strings
+  * @s1: char
+  * @s2: char
+  * @n: unsigned int
+  * Return: char
+  */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *slit;
+	unsigned int ls1, ls2, lslit, i;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
+		;
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+		;
+	if (n > ls2)
+		n = ls2;
+	lslit = ls1 + n;
+	slit = malloc(lslit + 1);
+	if (slit == NULL)
+		return (NULL);
+	for (i = 0; i < lslit; i++)
+		if (i < ls1)
+			slit[i] = s1[i];
+		else
+			slit[i] = s2[i - ls1];
+	slit[i] = '\0';
+	return (slit);
+}
