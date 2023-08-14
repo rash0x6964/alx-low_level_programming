8d16d1d74f16165560296aaf1da2d9ee714e918c
rash0x6964 <rachidbnou572@gmail.com>
2023-08-14 01:56:14 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+# define DOG_H
+
+/**
+ *struct dog - New data type as dog
+ *@name: dog name
+ *@age: dog age
+ *@owner: dog's owner's name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
