6fbecc820f379ede6fac20d179337ffbd1e60f2d
rash0x6964 <rachidbnou572@gmail.com>
2023-08-16 01:52:16 -0700

    task 3

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,8 @@
 #ifndef FUN_POINTERS_H
+# define FUN_POINTERS_H
+
+# include <stddef.h>
+# include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
