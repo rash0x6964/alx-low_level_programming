4d654ded11329a5e15f3dd93437f83a3adfa423f
rash0x6964 <rachidbnou572@gmail.com>
2023-08-17 06:00:08 -0700

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,12 +12,13 @@ void print_all(const char * const format, ...)
 {
 	va_list ap;
 	unsigned int i = 0;
+	int start = 0;
 	char *s;
 
 	va_start(ap, format);
 	while (format && format[i])
 	{
-		if (i > 0 && (format[i] == 'c' || format[i] == 'i'
+		if (start && (format[i] == 'c' || format[i] == 'i'
 			     || format[i] == 'f' || format[i] == 's'))
 		{
 			printf(", ");
@@ -26,14 +27,18 @@ void print_all(const char * const format, ...)
 		{
 			case 'c':
 				printf("%c", (char)va_arg(ap, int));
+				start = 1;
 				break;
 			case 'i':
 				printf("%d", va_arg(ap, int));
+				start = 1;
 				break;
 			case 'f':
 				printf("%f", (float)va_arg(ap, double));
+				start = 1;
 				break;
 			case 's':
+				start = 1;
 				s = va_arg(ap, char*);
 				if ((s && printf("%s", s)) || printf("(nil)"))
 				{}
