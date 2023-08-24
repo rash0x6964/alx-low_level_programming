066ba90ef5aba02fdfd8e54a8da304e619b3a392
rash0x6964 <rash0x6964@gmail.com>
2023-08-24 04:48:18 -0700

    ...

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,7 +13,7 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str)
-			printf("[%d] (%s)\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		else
 			printf("[0] (nil)\n");
 		count++;
