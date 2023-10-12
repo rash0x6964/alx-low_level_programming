9ef7136bd811a0ea754883f820dea08c8fbbca6c
rash0x6964 <rash0x6964@gmail.com>
2023-10-12 02:49:18 -0700

    task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -2,13 +2,13 @@
 
 /**
  * get_dnodeint_at_index - function that returns the nth node of a list
- * @head: the first node 
- * @index: index of the node, starting form 0 
+ * @head: the first node
+ * @index: index of the node, starting form 0
  * Return: the node or NULL
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    size_t count = 0;
+	size_t count = 0;
 
 	while (head != NULL)
 	{
