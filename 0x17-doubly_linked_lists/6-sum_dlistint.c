83631f96ffb6b694c1dbe4d7e01cd4cd8f8c3c23
rash0x6964 <rash0x6964@gmail.com>
2023-10-12 02:50:31 -0700

    task 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - function that returns the sum of a list
+ * @head: the first node
+ * Return: the sum or 0
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
