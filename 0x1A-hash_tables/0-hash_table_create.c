403c7b1b62883380e4250b970ab704044a16ee24
rash0x6964 <rash0x6964@gmail.com>
2024-02-02 08:55:08 +0100

    fix task 0 error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,8 +10,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
 
-	table = calloc(sizeof(hash_table_t), size);
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
 	table->size = size;
-	table->array = NULL;
+	table->array = calloc(size, sizeof(hash_node_t *));
+	if (!table->array)
+	{
+		free(table);
+		return (NULL);
+	}
 	return (table);
 }
